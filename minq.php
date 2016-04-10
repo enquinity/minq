e897ccc38ee2cc70a91e12150e412366368d8baa
Enquinity <enquinity@gmail.com>
2016-04-10 10:20:26 +0200

    Dependency Container Interfaces

--- minq.php
+++ minq.php
@@ -2,10 +2,24 @@
 /**
  * Minquinity App Framework version 1.0.0
  */
- namespace Minq;
+namespace minq;
+
+interface IDependencyFactory {
+    public function createDependency($type, IDependencyContainer $container);
+    public function getDependencyFlags($type);
+}
  
- class Controller {
- }
+interface IDependencyContainer {
+    public function resolve($type);
+    public function injectInto($object);
+    public function registerInstance($type, $instance);
+    public function registerFactory(IDependencyFactory $factory);
+    public function register($type, $createCallback, $flags);
+    public function registerClass($type, $implementingClassName, array $constructorParams = [], $flags);
+}
  
- class Application {
- }
+class Controller {
+}
+ 
+class Application {
+}
